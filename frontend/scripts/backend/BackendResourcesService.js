7b7caaa8367f870b5b3d8e56791dc460cf640555
Ken Lee <kdblitz@gmail.com>
2016-07-18 01:15:12 +0800

    Added more backend dependencies.

--- frontend/scripts/backend/BackendResourcesService.js
+++ frontend/scripts/backend/BackendResourcesService.js
@@ -1,23 +1,9 @@
 import module from './module';
+import resources from './backendResources';
 
 class BackendResourcesService {
   constructor () {
-    this.tools = [{
-      name: 'Node.js',
-      image: 'images/node.png',
-      description: [
-        'javascript engine used to run the backend application',
-        'can support <abbr title="ECMAScript 6">ES6</abbr> features(more common ones) as of v4.0',
-        'comes pre-installed with <abbr title="Node package manager">NPM</abbr>'
-      ],
-      references: [{
-        name: 'Node.JS Documentation',
-        link: 'https://nodejs.org/en/docs/'
-      }, {
-        name: 'Cool visual explaination of NPM',
-        link: 'https://unpm.nodesource.com/'
-      }]
-    }];
+    this.tools = resources;
   }
 
   getBackendTools () {
---
+++ frontend/scripts/backend/backendResources.js
@@ -0,0 +1,71 @@
+export default [{
+  name: 'Node.js',
+  image: 'images/node.png',
+  description: [
+    'javascript engine used to run the backend application',
+    'comes pre-installed with <abbr title="Node package manager">NPM</abbr>',
+    'can support <abbr title="ECMAScript 6">ES6</abbr> features(more common ones) as of v4.0'
+  ],
+  references: [{
+    name: 'Node.JS Documentation',
+    link: 'https://nodejs.org/en/docs/'
+  }, {
+    name: 'Cool visual explaination of NPM',
+    link: 'https://unpm.nodesource.com/'
+  }, {
+    name: 'NPM Registry',
+    link: 'https://www.npmjs.com/'
+  }]
+}, {
+  name: 'Express',
+  image: 'images/express.png',
+  description: [
+    'serves the static files of the application',
+    'also defines routing',
+    'may use middlewares to help in session management, parsing post parameters, etc',
+    'can be used to define your own API\'s'
+  ],
+  references: [{
+    name: 'Express site',
+    link: 'https://expressjs.com/'
+  }, {
+    name: 'Routing guide',
+    link: 'https://expressjs.com/en/guide/routing.html'
+  }]
+}, {
+  name: 'Webpack',
+  image: 'images/webpack.gif',
+  description: [
+    'module bundler',
+    'uses loaders as extensions to support different post-processing'
+  ],
+  references: [{
+    name: 'Webpack site',
+    link: 'https://webpack.github.io/'
+  }]
+}, {
+  name: 'Semistandard',
+  image: 'images/semistandard.svg',
+  description: [
+    'linting tool',
+    'defines the eslint rules so that you won\'t need to manage it',
+    'uses <a href="http://standardjs.com/rules.html" target="_blank">Standard style</a> + semicolons'
+  ],
+  references: [{
+    name: 'Semistandard link',
+    link: 'https://github.com/Flet/semistandard',
+  }]
+}, {
+  name: 'Bower',
+  image: 'images/bower.png',
+  description: [
+    'manages frontend dependencies of the application'
+  ],
+  references: [{
+    name: 'Bower commands',
+    link: 'https://bower.io/docs/api/'
+  }, {
+    name: 'Bower registry',
+    link: 'https://bower.io/search/'
+  }]
+}];
