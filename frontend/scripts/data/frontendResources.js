35b0a77816dbf6d85fa2aec5cd8457656c9d77cb
Ken Lee <ken.lee@nokia.com>
2016-07-19 18:46:26 +0800

    Added frontend resources.

--- frontend/scripts/data/frontendResources.js
+++ frontend/scripts/data/frontendResources.js
@@ -1,1 +1,62 @@
-export default [];
+export default [{
+  name: 'AngularJS',
+  image: 'images/angular.png',
+  description: [
+    'currently using 1.4 (but will be transitioning to 1.5 and eventually 2.0)'
+  ],
+  references: [{
+    name: 'Angular site',
+    link: 'https://angularjs.org/'
+  }, {
+    name: 'Angular 1 documentation'
+  }]
+}, {
+  name: 'UI Router',
+  image: 'images/ui-router.png',
+  description: [
+    'routing framework for AngularJS',
+    'routes organized as a state machine',
+    'allows nested states'
+  ],
+  references: [{
+    name: 'UI Router Documentation',
+    link: 'http://angular-ui.github.io/ui-router/site/#/api/ui.router'
+  }]
+}, {
+  name: 'jQuery',
+  image: 'images/jquery.svg'
+}, {
+  name: 'Bootstrap',
+  image: 'images/bootstrap.png',
+  description: [
+    `well not completely<span title="we are using an customized version for Nokia">...</span>`,
+    'manages the layout and overall look and feel of the site'
+  ],
+  references: [{
+    name: 'Bootstrap documentation',
+    link: 'http://getbootstrap.com/css/'
+  }]
+}, {
+  name: 'FuelUX',
+  image: 'images/fuelux.png',
+  description: [
+    'additional jQuery plugins for additional components'
+  ],
+  references: [{
+    name: 'FuelUX Components',
+    link: 'http://getfuelux.com/javascript.html'
+  }]
+}, {
+  name: 'Lodash',
+  image: 'images/lodash.png',
+  description: [
+    'collection of utility functions handling javascript objects or collections'
+  ],
+  references: [{
+    name: 'Lodash documentation',
+    link: 'https://lodash.com/docs'
+  }, {
+    name: '10 Javascript Utility Functions That You Should Probably Stop Rewriting',
+    link: 'http://colintoh.com/blog/lodash-10-javascript-utility-functions-stop-rewriting'
+  }]
+}];
